b005a5b30d5dc479628a96bfb705ee88c066f66c
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2023-02-19 20:11:29 +0330

    a test for writing cipher text

--- Encypter.py
+++ Encypter.py
@@ -1,3 +1,9 @@
 f = open('test.txt', 'r')
 write = open('write.txt', 'w')
-write.write('hello')
+t = f.read()
+t = t.upper()
+for i in t:
+    if i == 'A' or i == 'B' or i == 'C' or i == 'D' or i == 'E' or i == 'F' or i == 'G' or i == 'H' or i == 'I' or \
+        i == 'J' or i == 'K' or i == 'L' or i == 'M' or i == 'N' or i == 'O' or i == 'P' or i == 'Q' or i == 'R' or \
+        i == 'S' or i == 'T' or i == 'U' or i == 'V' or i == 'W' or i == 'X' or i == 'Y' or i == 'Z':
+        write.write(i)
